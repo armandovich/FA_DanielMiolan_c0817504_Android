670252ce934c512a30fa4a77aa1e305b24eda41d
Daniel Miolan <miolan96@gmail.com>
2022-02-14 22:26:09 -0400

    Get user location.

--- app/src/main/java/com/example/fa_danielmiolan_c0817504_android/MapsActivity.java
+++ app/src/main/java/com/example/fa_danielmiolan_c0817504_android/MapsActivity.java
@@ -1,21 +1,33 @@
 package com.example.fa_danielmiolan_c0817504_android;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.os.Bundle;
-
+import com.example.fa_danielmiolan_c0817504_android.databinding.ActivityMapsBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.fa_danielmiolan_c0817504_android.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private FusedLocationProviderClient locationProviderClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private boolean didZoomToUser = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     /**
@@ -49,14 +64,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 markerOptions.position(latLng);
                 markerOptions.title(latLng.latitude + " : " + latLng.longitude);
                 mMap.clear();
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
                 mMap.addMarker(markerOptions);
             }
         });
 
+        if (hasLocationPermission()) {
+            updateUserLocation();
+        }
         // Add a marker in Sydney and move the camera
         //LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private boolean hasLocationPermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void updateUserLocation() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                mMap.clear();
+
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(userLocation).title("your location!"));
+
+                    if (!didZoomToUser) {
+                        didZoomToUser = true;
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 10));
+                    }
+                }
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
     }
 }
