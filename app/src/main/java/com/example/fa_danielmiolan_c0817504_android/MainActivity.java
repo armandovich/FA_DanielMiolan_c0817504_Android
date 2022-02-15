da5a16f500cc28dd7878deac7db80b70fb3572ec
Daniel Miolan <miolan96@gmail.com>
2022-02-14 20:20:07 -0400

    Add and setup google maps.

--- app/src/main/java/com/example/fa_danielmiolan_c0817504_android/MainActivity.java
+++ app/src/main/java/com/example/fa_danielmiolan_c0817504_android/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.fa_danielmiolan_c0817504_android;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button openMapBtn;
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openMapBtn =  findViewById(R.id.openMapBtn);
+
+        openMapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TO-DO
+                Intent myIntent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, REQUEST_CODE);
     }
 }
